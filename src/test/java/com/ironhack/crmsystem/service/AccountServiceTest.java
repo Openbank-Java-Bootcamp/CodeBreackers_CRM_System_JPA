fb5d6e61a9a15fb9f8c416e8e408890738a73c8c
pilaraf <pilaralvarezfournier@gmail.com>
2022-05-12 21:51:51 +0200

    test

--- src/test/java/com/ironhack/crmsystem/service/AccountServiceTest.java
+++ src/test/java/com/ironhack/crmsystem/service/AccountServiceTest.java
@@ -3,21 +3,28 @@ package com.ironhack.crmsystem.service;
 import com.ironhack.crmsystem.enums.Industry;
 import com.ironhack.crmsystem.model.Account;
 import com.ironhack.crmsystem.repository.AccountRepository;
+import nl.altindag.console.ConsoleCaptor;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class AccountServiceTest {
     @Autowired
     AccountRepository accountRepository;
+
+    @Autowired
+    AccountService accountService;
 
     @BeforeEach
     void setUp() {
@@ -34,9 +41,45 @@ class AccountServiceTest {
 
     @Test
     public void maxEmployeeCont_JPATest(){
-        List<Object[]> result = accountRepository.findMaxEmployeeCount();
-        assertEquals(50,result.get(0)[1]);
+        accountService.maxEmployeeCont();
+        StringReader sr = new StringReader("s");
+        Scanner scan = new Scanner(sr);
+        ConsoleCaptor consoleCaptor = new ConsoleCaptor();
+        List<String> s = consoleCaptor.getStandardOutput();
+        consoleCaptor.close();
+        assertThat(s.contains("50"));
     }
 
+    @Test
+    public void minEmployeeContJPATest(){
+        accountService.minEmployeeCont();
+        StringReader sr = new StringReader("s");
+        Scanner scan = new Scanner(sr);
+        ConsoleCaptor consoleCaptor = new ConsoleCaptor();
+        List<String> s = consoleCaptor.getStandardOutput();
+        consoleCaptor.close();
+        assertThat(s.contains("10"));
+    }
+    @Test
+    public void meanEmployeeCont(){
+        accountService.meanEmployeeCont();
+        StringReader sr = new StringReader("s");
+        Scanner scan = new Scanner(sr);
+        ConsoleCaptor consoleCaptor = new ConsoleCaptor();
+        List<String> s = consoleCaptor.getStandardOutput();
+        consoleCaptor.close();
+        assertThat(s.contains("26"));
+    }
+
+    @Test
+    public void medianEmployeeCont(){
+        accountService.medianEmployeeCont();
+        StringReader sr = new StringReader("s");
+        Scanner scan = new Scanner(sr);
+        ConsoleCaptor consoleCaptor = new ConsoleCaptor();
+        List<String> s = consoleCaptor.getStandardOutput();
+        consoleCaptor.close();
+        assertThat(s.contains("20"));
+    }
 
 }
